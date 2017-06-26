63b17f58afadc82fdb1cb63c18f1a9139560f893
Diego F.C <diego.fc@gmail.com>
2017-06-26 23:15:40 +0200

    Basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        modules: [
+            path.join(__dirname, 'src'),
+            'node_modules'
+        ]
+    },
+
+    entry: [
+        'webpack-dev-server/client',
+        'webpack/hot/only-dev-server',
+        path.join(__dirname, 'src', 'index.jsx')
+    ],
+
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {loader: 'babel-loader'}
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'}
+                ]
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {loader: 'file-loader'}
+                ]
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: [
+                    {loader: 'url-loader?prefix=font/&limit=5000'}
+                ]
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {loader: 'url-loader?limit=10000&mimetype=application/octet-stream'}
+                ]
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {loader: 'url-loader?limit=10000&mimetype=image/svg+xml'}
+                ]
+            }            
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'React Redux eCommerce',
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'index.html'
+        })
+    ],
+
+    devServer: {
+        host: '0.0.0.0',
+        hot: true,
+        port: 8080,
+        inline: true,
+        contentBase: path.join(__dirname, 'src'),
+        historyApiFallback: true
+    }
+
+};
